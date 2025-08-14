17a9753313cb3b971e27e323c01205c435a9f1cd
Mike <Mike@Michaels-MacBook-Air.local>
2025-08-13 17:01:01 -0700

    Fix module resolution issues in webpack and update Babel config for Flow types

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,9 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ["@babel/plugin-transform-private-methods", { "loose": true }],
-    ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+    ["@babel/plugin-transform-flow-strip-types"],
+    ["@babel/plugin-transform-class-properties", { "loose": true }]
   ],
   env: {
     production: {
